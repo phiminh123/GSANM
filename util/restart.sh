43772be3512e4ca8e1edb067c76785665ac691b0
Your Name <you@example.com>
2024-10-06 22:37:16 +0700

    loki

--- util/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-script=$0
-$(dirname "$script")/stop.sh
-sleep 5
-$(dirname "$script")/start.sh
--- util/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-script=$0
-docker stack deploy -c $(dirname "$script")/../docker-compose.yml prom
-sleep 3
-$(dirname "$script")/status.sh
